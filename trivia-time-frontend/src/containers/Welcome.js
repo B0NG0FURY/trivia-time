4f4def1d8b3d033866be792246383447174cafa4
bongofury <jtroutmanman@gmail.com>
2021-09-15 09:55:12 -0500

    added sign up button to Welcome container component

--- trivia-time-frontend/src/containers/Welcome.js
+++ trivia-time-frontend/src/containers/Welcome.js
@@ -6,7 +6,9 @@ export default class Welcome extends Component {
         return(
             <div>
                 <h1>Welcome To Trivia Time</h1>
-                <Button>Sign In</Button>
+                <Button>Log In</Button>
+                <br></br><br></br>
+                <Button>Sign Up</Button>
             </div>
         )
     }
