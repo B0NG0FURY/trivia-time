1a154d7f0f9c482676102d00bbd29796f5b020a6
bongofury <jtroutmanman@gmail.com>
2021-09-16 02:29:28 -0500

    installed react-router-dom. added routes for welcome, signin, and signup components. added links to buttons in welcome component.

--- trivia-time-frontend/src/App.js
+++ trivia-time-frontend/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import Welcome from './containers/Welcome';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Welcome />
+      <h1>Trivia Time</h1>
+      <Router>
+        <Route exact path="/" component={Welcome} />
+        <Route exact path="/login" component={SignIn} />
+        <Route exact path="/signup" component={SignUp} />
+      </Router>
     </div>
   );
 }
--- trivia-time-frontend/src/containers/Welcome.js
+++ trivia-time-frontend/src/containers/Welcome.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button';
-import SignIn from '../components/SignIn';
+import Link from 'react-router-dom/Link';
 
 export default class Welcome extends Component {
     render() {
         return(
             <div >
                 <h1>Welcome To Trivia Time</h1>
-                <Button>Log In</Button>
+                <Link to="/login">
+                    <Button>Log In</Button>
+                </Link>
                 <br></br><br></br>
-                <Button>Sign Up</Button>
-                <SignIn />
+                <Link to="/signup">
+                    <Button>Sign Up</Button>
+                </Link>
             </div>
         )
     }
