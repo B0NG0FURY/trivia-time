24f090516e3379ac25eafab4ce6f0a89272b0bc7
bongofury <jtroutmanman@gmail.com>
2021-09-17 14:04:38 -0500

    added /logout route.

--- trivia-time-backend/config/routes.rb
+++ trivia-time-backend/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: [:create]
   post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#delete'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
