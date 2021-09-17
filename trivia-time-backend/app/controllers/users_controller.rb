bb59a7f4531356af2bb3be9be525a73e05ff0a34
bongofury <jtroutmanman@gmail.com>
2021-09-17 10:56:52 -0500

    added code to create a new user, create a new session, and render json with user info.

--- trivia-time-backend/app/controllers/users_controller.rb
+++ trivia-time-backend/app/controllers/users_controller.rb
@@ -1,7 +1,20 @@
 class UsersController < ApplicationController
 
     def create
-        @user = User.create(user_params)
+        user = User.create(user_params)
+        if user.save
+            session[:user_id] = user.id
+            console.log(session[:user_id])
+            render json: {
+                status: :created,
+                user: user
+            }
+        else
+            render json: {
+                status: 500,
+                errors: user.errors.full_messages
+            }
+        end
     end
 
     private
