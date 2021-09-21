3238c7671e1fa7e8cfb8c6ad28c031e54137ba09
bongofury <jtroutmanman@gmail.com>
2021-09-21 17:42:44 -0500

    connected state and dispatch to props. added code to submit form data to backend and receive response. added Redirect component to redirect to UserComponent on successful creation of new account. added code to display errors if any occur upon account creation but still need to create function to create list elements for each error.

--- trivia-time-frontend/src/components/SignUp.js
+++ trivia-time-frontend/src/components/SignUp.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Form } from 'react-bootstrap';
+import { connect } from 'react-redux';
+import { addUser } from '../actions/addUser';
 
 class SignUp extends Component {
 
@@ -31,43 +33,79 @@ class SignUp extends Component {
                 }
             })
         }
-        fetch("http://localhost:3001/users", configObject).then(resp => resp.json()).then(info => console.log(info))
+        fetch("http://localhost:3001/users", configObject).then(resp => resp.json()).then(resp => {
+            if (resp.user) {
+                let user = {
+                    logged_in: resp.logged_in,
+                    user: resp.user.username,
+                    id: resp.user.id
+                };
+                this.props.addUser(user);
+                this.setState({
+                    ...this.state,
+                    redirect: "/user"
+                });
+            } else {
+                this.setState({
+                    ...this.state,
+                    errors: resp.errors
+                })
+            }
+        })
     }
 
     render() {
         return(
-            <Form onSubmit={this.handleSubmit} >
-                <Form.Group>
-                    <Form.Control
-                        type="text"
-                        name="username"
-                        placeholder="Username"
-                        onChange={this.handleOnChange}
-                        value={this.state.username}
-                    />
-                </Form.Group>
-                <Form.Group>
-                    <Form.Control
-                        type="password"
-                        name="password"
-                        placeholder="Password"
-                        onChange={this.handleOnChange}
-                        value={this.state.password}
-                    />
-                </Form.Group>
-                <Form.Group>
-                    <Form.Control
-                        type="password"
-                        name="passwordConfirmation"
-                        placeholder="Confirm Password"
-                        onChange={this.handleOnChange}
-                        value={this.state.passwordConfirmation}
-                    />
-                </Form.Group>
-                <Form.Control type="submit" value="Create Account" />
-            </Form>
+            <div>
+                { this.state.errors ? this.handleErrors : null}
+                <Form onSubmit={this.handleSubmit} >
+                    <Form.Group>
+                        <Form.Control
+                            type="text"
+                            name="username"
+                            placeholder="Username"
+                            onChange={this.handleOnChange}
+                            value={this.state.username}
+                        />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Control
+                            type="password"
+                            name="password"
+                            placeholder="Password"
+                            onChange={this.handleOnChange}
+                            value={this.state.password}
+                        />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Control
+                            type="password"
+                            name="passwordConfirmation"
+                            placeholder="Confirm Password"
+                            onChange={this.handleOnChange}
+                            value={this.state.passwordConfirmation}
+                        />
+                    </Form.Group>
+                    <Form.Control type="submit" value="Create Account" />
+                </Form>
+                { this.state.redirect ? <Redirect to={this.state.redirect} /> : null }
+            </div>
         )
     }
 }
 
-export default SignUp;
+const mapStateToProps = state => {
+    return {
+        user: state
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        addUser: (user) => {
+            dispatch(addUser(user))
+        }
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignUp);
