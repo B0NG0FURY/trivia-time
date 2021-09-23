edb5ffb1738e4cb488f1d0b7f71cdec4a6d6a7c9
bongofury <jtroutmanman@gmail.com>
2021-09-22 19:43:01 -0500

    added select elements for category, difficulty, and number of questions to form for new game.

--- trivia-time-frontend/src/components/NewGame.js
+++ trivia-time-frontend/src/components/NewGame.js
@@ -13,7 +13,43 @@ class NewGame extends Component {
             <div>
                 <h3>Start A New Game</h3>
                 <Form>
-                    
+                    <Form.Group>
+                        <Form.Label>Category</Form.Label>
+                        <Form.Select>
+                            <option>Select Category</option>
+                            <option value="">Any Category</option>
+                            <option value="9">General Knowledge</option>
+                            <option value="10">Books</option>
+                            <option value="11">Film</option>
+                            <option value="12">Music</option>
+                            <option value="14">TV</option>
+                            <option value="15">Video Games</option>
+                            <option value="17">Science/Nature</option>
+                            <option value="21">Sports</option>
+                            <option value="23">History</option>
+                            <option value="25">History</option>
+                            <option value="27">Animals</option>
+                        </Form.Select>
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Difficulty</Form.Label>
+                        <Form.Select>
+                            <option>Select Difficulty</option>
+                            <option value="easy">Easy</option>
+                            <option value="medium">Medium</option>
+                            <option value="hard">Hard</option>
+                        </Form.Select>
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Number Of Questions</Form.Label>
+                        <Form.Select>
+                            <option>Select Amount</option>
+                            <option value="5">5</option>
+                            <option value="10">10</option>
+                            <option value="15">15</option>
+                            <option value="20">20</option>
+                        </Form.Select>
+                    </Form.Group>
                 </Form>
             </div>
         )
