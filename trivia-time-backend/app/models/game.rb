2a100ea81050c80d1812fbe6fd2e1acc3590da84
bongofury <jtroutmanman@gmail.com>
2021-09-25 10:59:26 -0500

    added validations to game and question models. game model now accepts nested attributes for questions.

--- trivia-time-backend/app/models/game.rb
+++ trivia-time-backend/app/models/game.rb
@@ -2,4 +2,7 @@ class Game < ApplicationRecord
     belongs_to :user
     belongs_to :category
     has_and_belongs_to_many :questions
+    accepts_nested_attributes_for :questions
+    validates :category, presence: true
+    validates :difficulty, presence: true
 end
--- trivia-time-backend/app/models/question.rb
+++ trivia-time-backend/app/models/question.rb
@@ -1,3 +1,5 @@
 class Question < ApplicationRecord
     has_and_belongs_to_many :games
+    validates :text, presence: true
+    validates :correct_answer, presence: true
 end
