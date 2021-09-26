2ca3dddc6ae3b1ce422940d9187ef26a05c3ca7f
bongofury <jtroutmanman@gmail.com>
2021-09-25 20:32:31 -0500

    created all_answers method to randomly insert the correct answer into an array with the incorrect answers and return this new array.

--- trivia-time-backend/app/models/question.rb
+++ trivia-time-backend/app/models/question.rb
@@ -2,4 +2,9 @@ class Question < ApplicationRecord
     has_and_belongs_to_many :games
     validates :text, presence: true
     validates :correct_answer, presence: true
+
+    def all_answers
+        answers = self.incorrect_answers.map {|answer| answer}
+        answers.insert(rand(0..3), self.correct_answer)
+    end
 end
