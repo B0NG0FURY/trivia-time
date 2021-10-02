a9d58d603cda46ade7286e85cdd0bdb1d5cd7dad
bongofury <jtroutmanman@gmail.com>
2021-10-01 19:57:02 -0500

    mapped state to props of GameContainer. added conditional statement to show Game Loading message while it is awaiting the new game data.

--- trivia-time-frontend/src/containers/GameContainer.js
+++ trivia-time-frontend/src/containers/GameContainer.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
+import { connect } from  'react-redux';
 
 class GameContainer extends Component {
-
+    render() {
+        return(
+            <div>
+                this.props.game.receiving ? <h1>Loading Game...</h1> : null
+            </div>
+        )
+    }
 }
 
-export default GameContainer;
+function mapStateToProps(state) {
+    return { game: state.game }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(GameContainer);
