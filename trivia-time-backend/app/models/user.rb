489428693623fff6b50a750c2cc4eba094aa6517
bongofury <jtroutmanman@gmail.com>
2021-10-17 21:57:00 -0500

    added password length validation.

--- trivia-time-backend/app/models/user.rb
+++ trivia-time-backend/app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
     has_secure_password
     validates :username, presence: true
     validates :username, uniqueness: true
+    validates :password, length: { in: 3..20 }
 
     def games_played
         self.games.length
